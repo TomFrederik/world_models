19ec2bb83462e763c27fb35213ce92bdcf1a3aff
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-10-16 19:52:10 +0000

    m

--- collect_random_rollouts.py
+++ collect_random_rollouts.py
@@ -2,15 +2,17 @@
 This script samples 10,000 random rollouts on the carracing environment
 """
 
-
 import gym
+from gym import wrappers
 import numpy as np
 import argparse
 
 def main(id, number_rollouts):
     env = gym.make('CarRacing-v0')
-
+    env = wrappers.Monitor(env, "./gym_results",force=True)
+   
     rollouts = []
+    counter = 1
 
     for _ in range(number_rollouts):
 
@@ -34,7 +36,13 @@ def main(id, number_rollouts):
         
         # save rollout
         rollouts.append((actions, observations))
-
+        
+        counter += 1
+        
+        # save progress so far
+        if counter%100 == 0:
+            np.save('random_rollouts_{}.npy'.format(id), rollouts)
+    
     env.close()
 
     np.save('random_rollouts_{}.npy'.format(id), rollouts)
